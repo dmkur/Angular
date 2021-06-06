cc47fa85ec097889fa080f6e48ad547fc1855a51
dmkur <fgameua@gmail.com>
2021-06-06 22:16:46 +0300

    les2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
+import {AppComponent} from './app.component';
+import {UsersComponent} from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
-import { UserComponent } from './components/user/user.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
+import {UserComponent} from './components/user/user.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {PostComponent} from './components/post/post.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from './components/home/home.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+let routes:Routes = [
+  {path: `home`, component: HomeComponent},
+  {
+    path: `users`, component: UsersComponent,
+    children: [{path: ':id', component: UserDetailsComponent}]
+  },
+  {path: `posts`, component: PostsComponent},
+]
 
 @NgModule({
   declarations: [
@@ -15,12 +26,17 @@ import { PostComponent } from './components/post/post.component';
     UserComponent,
     PostsComponent,
     PostComponent,
+    HomeComponent,
+    UserDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {User} from "../../models/User";
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent implements OnInit {
+  user: User
+
+  constructor(private router: Router, private activateRoute: ActivatedRoute) {
+    this.activateRoute.params.subscribe(() => {
+      this.user = this.router.getCurrentNavigation()?.extras.state as User;
+    })
+  }
+
+  ngOnInit(): void {
+    console.log(history.state);
+  }
+
+}
